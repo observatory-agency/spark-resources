1c760faa852890f13e7485ca56223c55cae1706e
Pete Lloyd <plloyd11@gmail.com>
2023-02-13 18:38:30 -0500

    landing page updates

--- src/storyblok/lpCardGrid.svelte
+++ src/storyblok/lpCardGrid.svelte
@@ -4,7 +4,11 @@
 </script>
 
 <section use:storyblokEditable={blok} class="px-4 mx-auto max-w-screen-2xl">
-	<div class="grid gap-6 {blok.number_of_columns === '3' ? 'grid-cols-3' : 'grid-cols-4'}">
+	<div
+		class="grid gap-6 grid-cols-1 {blok.number_of_columns === '3'
+			? 'md:grid-cols-3'
+			: 'md:grid-cols-4'}"
+	>
 		{#each blok.cards as blok}
 			<StoryblokComponent {blok} />
 		{/each}
--- src/storyblok/lpSimpleHero.svelte
+++ src/storyblok/lpSimpleHero.svelte
@@ -5,107 +5,85 @@
 	$: resolvedRichText = renderRichText(blok.intro);
 </script>
 
-<div class="isolate bg-sparkPeach">
-	<div
-		class="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
-	>
+<section use:storyblokEditable={blok}>
+	<div class="relative py-24 overflow-hidden bg-gray-900 isolate sm:py-32">
+		<img
+			src={blok.background_image.filename}
+			alt=""
+			class="absolute inset-0 object-cover w-full h-full -z-10 mix-blend-soft-light"
+		/>
 		<svg
-			class="relative left-[calc(50%-11rem)] -z-10 h-[21.1875rem] max-w-none -translate-x-1/2 rotate-[30deg] sm:left-[calc(50%-30rem)] sm:h-[42.375rem]"
-			viewBox="0 0 1155 678"
-			fill="none"
-			xmlns="http://www.w3.org/2000/svg"
+			viewBox="0 0 1097 845"
+			aria-hidden="true"
+			class="hidden transform-gpu blur-3xl sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:w-[68.5625rem]"
 		>
 			<path
-				fill="url(#45de2b6b-92d5-4d68-a6a0-9b9b2abad533)"
-				fill-opacity=".15"
-				d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
+				fill="url(#10724532-9d81-43d2-bb94-866e98dd6e42)"
+				fill-opacity=".2"
+				d="M301.174 646.641 193.541 844.786 0 546.172l301.174 100.469 193.845-356.855c1.241 164.891 42.802 431.935 199.124 180.978 195.402-313.696 143.295-588.18 284.729-419.266 113.148 135.13 124.068 367.989 115.378 467.527L811.753 372.553l20.102 451.119-530.681-177.031Z"
 			/>
 			<defs>
 				<linearGradient
-					id="45de2b6b-92d5-4d68-a6a0-9b9b2abad533"
-					x1="1155.49"
-					x2="-78.208"
-					y1=".177"
-					y2="474.645"
+					id="10724532-9d81-43d2-bb94-866e98dd6e42"
+					x1="1097.04"
+					x2="-141.165"
+					y1=".22"
+					y2="363.075"
 					gradientUnits="userSpaceOnUse"
 				>
 					<stop stop-color="#E84853" />
+					<stop offset="1" stop-color="#D881AB" />
+				</linearGradient>
+			</defs>
+		</svg>
+		<svg
+			viewBox="0 0 1097 845"
+			aria-hidden="true"
+			class="absolute left-1/2 -top-52 -z-10 w-[68.5625rem] -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0"
+		>
+			<path
+				fill="url(#8ddc7edb-8983-4cd7-bccb-79ad21097d70)"
+				fill-opacity=".2"
+				d="M301.174 646.641 193.541 844.786 0 546.172l301.174 100.469 193.845-356.855c1.241 164.891 42.802 431.935 199.124 180.978 195.402-313.696 143.295-588.18 284.729-419.266 113.148 135.13 124.068 367.989 115.378 467.527L811.753 372.553l20.102 451.119-530.681-177.031Z"
+			/>
+			<defs>
+				<linearGradient
+					id="8ddc7edb-8983-4cd7-bccb-79ad21097d70"
+					x1="1097.04"
+					x2="-141.165"
+					y1=".22"
+					y2="363.075"
+					gradientUnits="userSpaceOnUse"
+				>
+					<stop stop-color="#E69649" />
 					<stop offset="1" stop-color="#E69649" />
 				</linearGradient>
 			</defs>
 		</svg>
-	</div>
-	<section use:storyblokEditable={blok}>
-		<div class="relative px-6 lg:px-8">
-			<div class="mx-auto max-w-4xl pt-20 pb-32 sm:pt-48 sm:pb-40">
-				<div>
-					{#if blok.announcement_text}
-						<div class="hidden sm:mb-8 sm:flex sm:justify-center">
-							<div
-								class="relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20"
-							>
-								<span class="text-gray-600">
-									{blok.announcement_text}
-									<a href={blok.announcement_link} class="font-semibold text-indigo-600"
-										><span
-											class="absolute inset-0"
-											aria-hidden="true"
-										/>{blok.announcement_link_text}
-										<span aria-hidden="true">&rarr;</span></a
-									>
-								</span>
-							</div>
-						</div>
-					{/if}
-
-					<div class="flex flex-col justify-center space-y-8">
-						<div>
-							<h1
-								class="text-4xl text-sparkHeader font-bold tracking-tight sm:text-center sm:text-6xl"
-							>
-								{blok.title}
-							</h1>
-							<p class="mt-6 max-w-2xl mx-auto text-lg text-sparkBody/80 sm:text-center">
-								{@html resolvedRichText}
-							</p>
-						</div>
-						<a
-							href={blok.button_link}
-							class="inline-block max-w-[200px] mx-auto px-6 py-3 text-base font-medium text-center text-white border border-transparent rounded-md shadow bg-sparkRaspberry hover:bg-rose-600 focus:outline-none focus:ring-2 focus:ring-rose-500 focus:ring-offset-2 sm:px-10"
-							>{blok.button_text}</a
+		<div class="px-6 mx-auto max-w-7xl lg:px-8">
+			<div class="mx-auto max-w-7xl lg:mx-0">
+				{#if blok.announcement_text}
+					<div class="hidden sm:mb-8 sm:flex sm:justify-center">
+						<div
+							class="relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20"
 						>
-					</div>
-					<div
-						class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-					>
-						<svg
-							class="relative left-[calc(50%+3rem)] h-[21.1875rem] max-w-none -translate-x-1/2 sm:left-[calc(50%+36rem)] sm:h-[42.375rem]"
-							viewBox="0 0 1155 678"
-							fill="none"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								fill="url(#ecb5b0c9-546c-4772-8c71-4d3f06d544bc)"
-								fill-opacity=".2"
-								d="M317.219 518.975L203.852 678 0 438.341l317.219 80.634 204.172-286.402c1.307 132.337 45.083 346.658 209.733 145.248C936.936 126.058 882.053-94.234 1031.02 41.331c119.18 108.451 130.68 295.337 121.53 375.223L855 299l21.173 362.054-558.954-142.079z"
-							/>
-							<defs>
-								<linearGradient
-									id="ecb5b0c9-546c-4772-8c71-4d3f06d544bc"
-									x1="1155.49"
-									x2="-78.208"
-									y1=".177"
-									y2="474.645"
-									gradientUnits="userSpaceOnUse"
+							<span class="text-gray-600">
+								{blok.announcement_text}
+								<a href={blok.announcement_link} class="font-semibold text-indigo-600"
+									><span class="absolute inset-0" aria-hidden="true" />{blok.announcement_link_text}
+									<span aria-hidden="true">&rarr;</span></a
 								>
-									<stop stop-color="#E84853" />
-									<stop offset="1" stop-color="#D881AB" />
-								</linearGradient>
-							</defs>
-						</svg>
+							</span>
+						</div>
 					</div>
-				</div>
+				{/if}
+				<h2 class="text-4xl font-bold tracking-tight text-center text-white sm:text-6xl">
+					{blok.title}
+				</h2>
+				<p class="mt-6 text-lg leading-8 text-center text-gray-300">
+					{@html resolvedRichText}
+				</p>
 			</div>
 		</div>
-	</section>
-</div>
+	</div>
+</section>
